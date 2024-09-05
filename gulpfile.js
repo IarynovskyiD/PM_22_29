3fa525bab25f36ece21181d179645bd1ab9d6e8a
Iarynovskyi <dimayar20061@gmail.com>
2024-09-05 21:31:45 +0300

    4 commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,15 @@ gulp.task('testTask', function (done) {
     console.log('This is a test task!');
     done(); // Завершуємо таск викликом функції done
 });
+//додаткові плагіни Gulp
+let sass = require ("gulp-sass"), //конвертує SASS в CSS
+    cssnano = require ("gulp-cssnano"), //мінімізація CSS
+    autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
+    //CSS для підтримки
+    //старих браузерів
+    imagemin = require ('gulp-imagemin'), //стиснення зображень
+    concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
+    uglify = require ("gulp-uglify"), //мінімізація javascript
+    rename = require ("gulp-rename"); //перейменування файлів
 // Запуск тасків за замовчуванням
 gulp.task('default', gulp.series('testTask'));
