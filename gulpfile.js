619b07baa88adbc1f64fbee4afad8644f0c17874
Iarynovskyi <dimayar20061@gmail.com>
2024-09-05 21:14:35 +0300

    3 commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Підключаємо gulp
-var gulp = require("gulp");
+let gulp = require("gulp");
 // Створюємо тестовий таск
 gulp.task('testTask', function (done) {
     console.log('This is a test task!');
