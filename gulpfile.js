e5c961a5a0ca2996aa9d63d8eb4180ec1d0f286d
IarynovskyiD <dmytro.yarynovskyi.pm.2023@lpnu.ua>
2024-09-06 09:44:52 +0300

    lab1 finish

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,6 @@
 // Підключаємо gulp
-/*let gulp = require("gulp");
-// Створюємо тестовий таск
-gulp.task('testTask', function (done) {
-    console.log('This is a test task!');
-    done(); // Завершуємо таск викликом функції done
-});
-//додаткові плагіни Gulp
-let sass = require ("gulp-sass"), //конвертує SASS в CSS
-    cssnano = require ("gulp-cssnano"), //мінімізація CSS
-    autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
-    //CSS для підтримки
-    //старих браузерів
-    imagemin = require ('gulp-imagemin'), //стиснення зображень
-    concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
-    uglify = require ("gulp-uglify"), //мінімізація javascript
-    rename = require ("gulp-rename"); //перейменування файлів
-// Запуск тасків за замовчуванням
-
-//копіювання HTML файлів в папку dist
-gulp.task ( "html", function () {
-    return gulp.src ( "src / *. html")
-        .pipe (gulp.dest ( "dist"));
-});
-//об'єднання, компіляція Sass в CSS, додавання префіксів і подальша мінімізація коду
-gulp.task ( "sass", function () {
-    return gulp.src ( "src / sass / *. sass")
-        .pipe (concat ( 'styles.sass'))
-        .pipe (sass ())
-        .pipe (autoprefixer ({
-            browsers: [ 'last 2 versions'],
-            cascade: false
-        }))
-        .pipe (cssnano ())
-        .pipe (rename ({suffix: '.min'}))
-        .pipe (gulp.dest ( "dist / css"));
-});
-//об'єднання і стиснення JS-файлів
-gulp.task ( "scripts", function () {
-    return gulp.src ( "src / js / *. js") //вихідна директорія файлів
-        .pipe (concat ( 'scripts.js')) // конкатенація js-файлів в один
-        .pipe (uglify ()) //стиснення коду
-        .pipe (rename ({suffix: '.min'})) //перейменування файлу з
-        //приставкою .min
-        .pipe (gulp.dest ( "dist / js")); // директорія продакшена
-});
-
-//cтискання зображень
-gulp.task ( 'imgs', function () {
-    return gulp.src ( "src / images /*.+ (jpg | jpeg | png | gif)")
-        .pipe (imagemin ({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            interlaced: true
-        }))
-        .pipe (gulp.dest ( "dist / images"))
-});
-//відстежування за змінами у файлах
-gulp.task ( "watch", function () {
-    gulp.watch ( "src / *. html", [ "html"]);
-    gulp.watch ( "src / js / *. js", [ "scripts"]);
-    gulp.watch ( "src / sass / *. sass", [ "sass"]);
-    gulp.watch ( "src / images /*.+ (jpg | jpeg | png | gif)", [ "imgs"]);
-});
-//Запуск тасків за замовчуванням
-gulp.task ( "default", [ "html", "sass", "scripts", "imgs", "watch"]);
 
 
-
-
-gulp.task('default', gulp.series('testTask'));*/
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import cssnano from 'gulp-cssnano';
@@ -77,30 +9,46 @@ import imagemin from 'gulp-imagemin';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
-import * as sassCompiler from 'sass';
+import browserSync from 'browser-sync'; // Add BrowserSync
+import {rimraf} from 'rimraf'; // Add Rimraf
 
-const compileSass = sass(sassCompiler);
+//import * as sassCompiler from 'sass';
+//const compileSass = sass(sassCompiler);
+
+//Use Rimraf to clean dist directory
+gulp.task('clean', function (cb) {
+    rimraf('dist', { glob: false }).then(() => cb()).catch(cb);});
+
+gulp.task('browserSync', function (cb) {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'//'app'
+        },
+    });
+    cb(); // сигналізуємо про завершення
+});
 
 gulp.task('html', function () {
-    return gulp.src('src/*.html')
+    return gulp.src('app/*.html')
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function () {
-    return gulp.src('src/sass/*.sass')
+    return gulp.src('app/sass/*.sass')
         .pipe(concat('styles.sass'))
-        .pipe(compileSass().on('error', compileSass.logError))
+        //.pipe(compileSass().on('error', compileSass.logError))
         .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
+            //overrideBrowserslist: ['last 2 versions'],
+            browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('src/js/*.js')
+    return gulp.src('app/js/*.js')
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
@@ -108,7 +56,7 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('imgs', function () {
-    return gulp.src('src/img/*.+(jpg|jpeg|png|gif)')
+    return gulp.src('app/img/*.+(jpg|jpeg|png|gif)')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
@@ -117,11 +65,11 @@ gulp.task('imgs', function () {
         .pipe(gulp.dest('dist/images'));
 });
 
-//gulp.task('watch', function () {
-//    gulp.watch('src/*.html', gulp.series('html'));
-//    gulp.watch('src/js/*.js', gulp.series('scripts'));
-//    gulp.watch('src/sass/*.sass', gulp.series('sass'));
-//    gulp.watch('src/img/*.+(jpg|jpeg|png|gif)', gulp.series('imgs'));
-//});
+gulp.task('watch', function () {
+    gulp.watch('app/*.html', gulp.series('html'));
+    gulp.watch('app/js/*.js', gulp.series('scripts'));
+    gulp.watch('app/sass/*.sass', gulp.series('sass'));
+    gulp.watch('app/img/*.+(jpg|jpeg|png|gif)', gulp.series('imgs'));
+});
 
-gulp.task('default', gulp.series('html', 'sass', 'scripts', 'imgs' /*, 'watch'*/));
+gulp.task('default', gulp.series('clean', gulp.series('browserSync', 'html', 'sass', 'scripts', 'imgs' , 'watch')));
